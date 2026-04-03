2b41d6dda2595c1b422b54c6812d493c7b6b94f1
Sotirios S Magionas <somagion@outlook.com>
2026-04-03 13:43:50 +0300

    Updated violentWord denylist

--- utility/violentWords.js
+++ utility/violentWords.js
@@ -1,2 +1,2 @@
 export const violentWords =
-  "killed|killing|kills|died|dead|death|deaths|slain|perished|fatality|fatalities|casualty|casualties|deceased|massacres|massacre";
+  "kill|killed|killing|kills|die|died|dead|death|deaths|slain|perished|fatality|fatalities|casualty|casualties|deceased|massacres|massacre|suicide";
